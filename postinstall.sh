6db3499da67d2f44868c26242a9f91fd13435725
Kurt Roberts <kurt.roberts@gmail.com>
2019-05-01 10:43:45 -0400

    Making final library path absolute

--- postinstall.sh
+++ postinstall.sh
@@ -6,7 +6,7 @@ chmod 644 /Library/LaunchDaemons/com.github.ryangball.nice_updater.plist
 chown root:wheel /Library/LaunchDaemons/com.github.ryangball.nice_updater_on_demand.plist
 chmod 644 /Library/LaunchDaemons/com.github.ryangball.nice_updater_on_demand.plist
 chown root:wheel /Library/Scripts/nice_updater.sh
-chmod 755 Library/Scripts/nice_updater.sh
+chmod 755 /Library/Scripts/nice_updater.sh
 
 # Start our LaunchDaemons
 /bin/launchctl load -w /Library/LaunchDaemons/com.github.ryangball.nice_updater.plist
