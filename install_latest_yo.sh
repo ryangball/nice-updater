3e9f07b5a966ee31b4400c8449d78a6e8489a80e
Peter Lewis <peter@peterlew.is>
2019-11-26 13:56:33 +0000

    Update install_latest_yo.sh

--- install_latest_yo.sh
+++ install_latest_yo.sh
@@ -3,11 +3,11 @@
 # Contributed by Kurt Roberts https://github.com/kurtroberts
 # Obtained from https://github.com/ryangball/nice-updater
 
-# Use this script to avoid having to package yo in your Jamf Pro
+# Use this script to avoid having to package Yo in your Jamf Pro
 # To configure, create a policy available on an ongoing basis,
 # triggered by a custom event called "yo" that runs this script as a payload.
 
-# Get the url of the latest 'Yo' release
+# Get the url of the latest Yo release
 latestYoReleaseURL=$(curl -s https://api.github.com/repos/sheagcraig/yo/releases/latest | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["assets"][0]["browser_download_url"];');
 
 # Copy that release to /tmp/yo.pkg
@@ -21,9 +21,9 @@ rm -f /tmp/yo.pkg
 
 # Verify installation
 if [[ -e /Applications/Utilities/yo.app ]]; then
-    echo "'Yo' installation succeeded; exiting."
+    echo "Yo installation succeeded; exiting."
     exit 0
 else
-    echo "'Yo' installation failed; exiting."
+    echo "Yo installation failed; exiting."
     exit 1
 fi
